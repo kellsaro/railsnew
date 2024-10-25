2438f379058532d352df35e807713b6e678203b6
Maykell Sánchez Romero <kellsaro@gmail.com>
2024-10-25 19:01:18 +0000

    Add fontawesome

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "@fortawesome/fontawesome-free"; // => https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.6.0/js/all.js
